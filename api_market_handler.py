47638764bb4d6a45d097b17b00bbd83cbb72eb3a
mullingsd <mullingsd@coventry.ac.uk>
2021-11-07 03:16:43 +0000

    Code Refactoring

--- api_market_handler.py
+++ api_market_handler.py
@@ -7,61 +7,64 @@ api_url = "https://tarkov-market.com/api/v1/"
 release_state = False
 
 if (release_state):
+    print("Release State = " + str(release_state) + "! API Connected, Market Data Up-To-Date")
     apiJsonObj = urlopen(api_url + "items/all?&" + api_key)
     fleaMarketData = json.load(apiJsonObj)
 else:
-    print("Release State = " + str(release_state) + "!, API Not Connected, Market Data Outdated\n")
+    print("Release State = " + str(release_state) + "!, API Not Connected, Market Data Outdated")
     tempFleaMarketData_Json_File = open("tempFleaMarketData.json", encoding="utf-8")
     fleaMarketData = json.load(tempFleaMarketData_Json_File)
+
+
 
 def RefreshFleaMarketData():
     if(release_state):
         json_obj = urlopen(api_url + "items/all?&" + api_key)
         fleaMarketData = json.load(json_obj)
 
-def WeaponNamePresent(userMessage):
-    for weaponItem in fleaMarketData:
-        if (weaponItem["tags"][0] == "Weapon"):
-            if (weaponItem["shortName"].replace("-", "").replace(" ", "").lower() in userMessage.replace("-", "").replace(" ", "").lower()):
+
+
+def isWeaponNamePresent(p_userMessage):
+    return ItemNamePresent(p_userMessage, "Weapon")
+def isAmmoNamePresent(p_userMessage):
+    return ItemNamePresent(p_userMessage, "Ammo")
+
+def getWeaponName(p_userMessage):
+    return ItemName(p_userMessage, "Weapon")
+def getAmmoName(p_userMessage):
+    return ItemName(p_userMessage, "Ammo")
+
+def getWeaponPrice(p_weaponName):
+    return ItemPrice(p_weaponName, "Weapon")
+def getAmmoPrice(p_ammoName):
+    return ItemPrice(p_ammoName, "Ammo")
+
+
+
+def ItemNamePresent(p_userMessage, p_itemType):
+    for marketItem in fleaMarketData:
+        if (marketItem["tags"][0] == p_itemType):
+            if (marketItem["shortName"].replace("-", "").replace(" ", "").lower() in p_userMessage.replace("-", "").replace(" ", "").lower()):
                 return True
     return False
-def GetWeaponName(userMessage):
-    for weaponItem in fleaMarketData:
-        if (weaponItem["tags"][0] == "Weapon"):
-            if (weaponItem["shortName"].replace("-", "").replace(" ", "").lower() in userMessage.replace("-", "").replace(" ", "").lower()):
-                return weaponItem["shortName"]
-    return "ERROR! ---Weapon name not present---"
-def GetWeaponPrice(weaponName):
+def ItemName(p_userMessage, p_itemType):
+    for marketItem in fleaMarketData:
+        if (marketItem["tags"][0] == p_itemType):
+            if (p_userMessage == "debug_PrintAll"):
+                print(marketItem["tags"])
+            if (marketItem["shortName"].replace("-", "").replace(" ", "").lower() in p_userMessage.replace("-", "").replace(" ", "").lower()):
+                return marketItem["shortName"]
+    return "ERROR! ---Item name not present---"
+def ItemPrice(p_itemName, p_itemType):
     RefreshFleaMarketData()
-    for weaponItem in fleaMarketData:
-        if (weaponItem["tags"][0] == "Weapon"):
-            if (weaponName == weaponItem["shortName"]):
-                return weaponItem["avg24hPrice"]
-    return "ERROR! ---Price not found---"
+    for marketItem in fleaMarketData:
+        if (marketItem["tags"][0] == p_itemType):
+            if (p_itemName == marketItem["shortName"]):
+                return marketItem["avg24hPrice"]
+    return "ERROR! ---Item price not found---"
 
-def AmmoNamePresent(userMessage):
-    for ammoItem in fleaMarketData:
-        if (ammoItem["tags"][0] == "Ammo"):
-            if (ammoItem["shortName"].replace("-", "").replace(" ", "").lower() in userMessage.replace("-", "").replace(" ", "").lower()):
-                return True
-    return False
-def GetAmmoName(userMessage):
-    for ammoItem in fleaMarketData:
-        if (ammoItem["tags"][0] == "Ammo"):
-            if (ammoItem["shortName"].replace("-", "").replace(" ", "").lower() in userMessage.replace("-", "").replace(" ", "").lower()):
-                return ammoItem["shortName"]
-    return "ERROR! ---Ammo name not present---"
-def GetAmmoPrice(ammoName):
-    RefreshFleaMarketData()
-    for ammoItem in fleaMarketData:
-        if (ammoItem["tags"][0] == "Ammo"):
-            if (ammoName == ammoItem["shortName"]):
-                return ammoItem["avg24hPrice"]
-    return "ERROR! ---Price not found---"
 
-def TestFunction():
-    userMessage = "cost of the mp133"
-    if (WeaponNamePresent(userMessage)):
-        print("The " + GetWeaponName(userMessage) + " costs " + str(GetWeaponPrice(GetWeaponName(userMessage))) + " Rubbles")
-    else:
-        print("Shit")
+
+def printAllItemName(p_itemType):
+    if (not release_state):
+        ItemName("debug_PrintAll", p_itemType)
--- tarkov_discord_chatbot.py
+++ tarkov_discord_chatbot.py
@@ -1,54 +1,55 @@
 import os
 import discord
 import api_market_handler
+from datetime import datetime
 
 client = discord.Client()
 def word_checker(message):
     keywords = ["price", "info", "stats"]
     for word in keywords:
-      if word in message.content:
-        return word
+        if word in message.content:
+            return word
     return "no procedure"
   
 
 @client.event
 async def on_ready():
-  print("we have logged in as {0.user}".format(client))
+    print("Logged in as {0.user}".format(client) + " @ " + datetime.now().strftime("%H:%M:%S"))
 
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-   return
-  else:
-    procedure = word_checker(message)
+    if message.author == client.user:
+        return
+    else:
+        procedure = word_checker(message)
     if procedure == "no procedure":
-     await message.channel.send("do nothing")
+        await message.channel.send("do nothing")
     else:
-      await eval(procedure + "(message)")
+        await eval(procedure + "(message)")
 async def price(message):
-  if message.author == client.user:
-   return
-  else:
-    if api_market_handler.WeaponNamePresent(message.content):
-      response = "The " + api_market_handler.GetWeaponName(str(message.content)) + " is worth " + str(api_market_handler.GetWeaponPrice(api_market_handler.GetWeaponName(str(message.content)))) + " Rubles"
-      await message.channel.send(response)
-    elif api_market_handler.AmmoNamePresent(message.content):
-      response = "The " + api_market_handler.GetAmmoName(str(message.content)) + " ammo type is worth " + str(api_market_handler.GetAmmoPrice(api_market_handler.GetAmmoName(str(message.content)))) + " Rubles"
-      await message.channel.send(response)
+    if message.author == client.user:
+        return
     else:
-      await message.channel.send("ERROR")
+        if api_market_handler.isWeaponNamePresent(message.content):
+            response = "The " + api_market_handler.getWeaponName(str(message.content)) + " is worth " + str(api_market_handler.getWeaponPrice(api_market_handler.getWeaponName(str(message.content)))) + " Rubles"
+            await message.channel.send(response)
+        elif api_market_handler.isAmmoNamePresent(message.content):
+            response = "The " + api_market_handler.getAmmoName(str(message.content)) + " ammo type is worth " + str(api_market_handler.getAmmoPrice(api_market_handler.getAmmoName(str(message.content)))) + " Rubles"
+            await message.channel.send(response)
+        else:
+            await message.channel.send("ERROR")
 
 async def info(message):
-  if message.author == client.user:
-   return
-  else:
-    await message.channel.send("info")
+    if message.author == client.user:
+        return
+    else:
+        await message.channel.send("info")
 
 async def stats(message):
-  if message.author == client.user:
-   return
-  else:
-    await message.channel.send("stats")
-  
+    if message.author == client.user:
+        return
+    else:
+        await message.channel.send("stats")
+ 
 client.run('ODk5NDQ2MTUxNzM5MjE1OTMy.YWy4gQ.PI_BS8TljX4jD8Hb-DToge-1nA8')
