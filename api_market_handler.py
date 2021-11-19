afd341c0ff954c80f4b944726175ee0ce2908389
mullingsd <mullingsd@coventry.ac.uk>
2021-11-19 03:17:01 +0000

    Refactored

--- api_market_handler.py
+++ api_market_handler.py
@@ -7,16 +7,17 @@ api_url = "https://tarkov-market.com/api/v1/"
 release_state = True
 
 if (release_state):
-    print("Release State = " + str(release_state) + "! API Connected, Market Data Up-To-Date")
     apiJsonFile = urlopen(api_url + "items/all?&" + api_key)
     marketData = json.load(apiJsonFile)
+    print("Release State = " + str(release_state) + "! API Connected, Market Data Up-To-Date")
 
     offlineMarketData_Json_File = open("offlineMarketData.json", "w", encoding="utf-8")
     json.dump(marketData, offlineMarketData_Json_File, ensure_ascii = False)
+    print("Notice: Offline Market Data Updated & Saved\n")
 else:
-    print("Release State = " + str(release_state) + "!, API Not Connected, Market Data Outdated")
     offlineMarketData_Json_File = open("offlineMarketData.json", encoding="utf-8")
     marketData = json.load(offlineMarketData_Json_File)
+    print("Release State = " + str(release_state) + "!, API Not Connected, Market Data Outdated")
 
 def RefreshmarketData():
     if(release_state):
@@ -47,7 +48,7 @@ def getItemPrice(p_itemName, p_marketType):
             elif (p_marketType == "fMarket"):
                 return marketItem["traderPriceCur"] + str(marketItem["avg24hPrice"])
             else:
-                return "ERROR! ---Item price type not specified---"
+                return "ERROR! ---Item market type not specified---"
     return "ERROR! ---Item price not found---"
 def getItemTraderName(p_itemName):
     for marketItem in marketData:
