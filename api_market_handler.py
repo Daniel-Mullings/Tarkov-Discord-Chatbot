9f51d07054a9dc8a26b79feadf6cdbc485721d8c
mullingsd <mullingsd@coventry.ac.uk>
2021-11-19 02:19:52 +0000

    Capability to store API Market Data when Online (Function: When program is offline it has access to most recently updated date)

--- api_market_handler.py
+++ api_market_handler.py
@@ -4,22 +4,24 @@ from urllib.request import urlopen
 api_key = "x-api-key=9w3MIVAGdu84n6Db"
 api_url = "https://tarkov-market.com/api/v1/"
 
-release_state = False
+release_state = True
 
 if (release_state):
     print("Release State = " + str(release_state) + "! API Connected, Market Data Up-To-Date")
-    apiJsonObj = urlopen(api_url + "items/all?&" + api_key)
-    fleaMarketData = json.load(apiJsonObj)
+    apiJsonFile = urlopen(api_url + "items/all?&" + api_key)
+    marketData = json.load(apiJsonFile)
+
+    offlineMarketData_Json_File = open("offlineMarketData.json", "w", encoding="utf-8")
+    json.dump(marketData, offlineMarketData_Json_File, ensure_ascii = False)
 else:
     print("Release State = " + str(release_state) + "!, API Not Connected, Market Data Outdated")
-    tempFleaMarketData_Json_File = open("tempFleaMarketData.json", encoding="utf-8")
-    fleaMarketData = json.load(tempFleaMarketData_Json_File)
+    offlineMarketData_Json_File = open("offlineMarketData.json", encoding="utf-8")
+    marketData = json.load(offlineMarketData_Json_File)
 
-def RefreshFleaMarketData():
+def RefreshmarketData():
     if(release_state):
         json_obj = urlopen(api_url + "items/all?&" + api_key)
-        fleaMarketData = json.load(json_obj)
-
+        marketData = json.load(json_obj)
 
 def itemMarket_In_userMessage(p_marketItemShortName, p_userMessage):
     if (p_marketItemShortName.replace("-", "").replace(" ", "").lower() in p_userMessage.replace("-", "").replace(" ", "").lower()):
@@ -27,18 +29,18 @@ def itemMarket_In_userMessage(p_marketItemShortName, p_userMessage):
     return False
 
 def isItemNamePresent(p_userMessage):
-    for marketItem in fleaMarketData:
+    for marketItem in marketData:
         if (itemMarket_In_userMessage(marketItem["shortName"], p_userMessage)):
             return True
     return False
 def getItemName(p_userMessage):
-    for marketItem in fleaMarketData:
+    for marketItem in marketData:
         if (itemMarket_In_userMessage(marketItem["shortName"], p_userMessage)):
             return marketItem["shortName"]
     return "ERROR! ---Item name not present---"
 def getItemPrice(p_itemName, p_marketType):
-    RefreshFleaMarketData()
-    for marketItem in fleaMarketData:
+    RefreshmarketData()
+    for marketItem in marketData:
         if (p_itemName == marketItem["shortName"]):
             if (p_marketType == "tMarket"):
                 return marketItem["traderPriceCur"] + str(marketItem["traderPrice"])
@@ -48,7 +50,7 @@ def getItemPrice(p_itemName, p_marketType):
                 return "ERROR! ---Item price type not specified---"
     return "ERROR! ---Item price not found---"
 def getItemTraderName(p_itemName):
-    for marketItem in fleaMarketData:
+    for marketItem in marketData:
         if (marketItem["shortName"] == p_itemName):
             return marketItem["traderName"]
     return "ERROR! ---Item not sold by any trader" 
