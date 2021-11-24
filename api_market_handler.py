678cb6e7594caf53fe7df1612263a46dbd38ea8d
mullingsd <mullingsd@coventry.ac.uk>
2021-11-24 12:42:53 +0000

    Exception Handling for API Errors Implements

--- api_market_handler.py
+++ api_market_handler.py
@@ -6,15 +6,20 @@ api_url = "https://tarkov-market.com/api/v1/"
 
 release_state = True
 
-if (release_state):
-    apiJsonFile = urlopen(api_url + "items/all?&" + api_key)
-    marketData = json.load(apiJsonFile)
-    print("Release State = " + str(release_state) + "! API Connected, Market Data Up-To-Date")
+def FetchOnlineMarketData():
+    try:
+        apiJsonFile = urlopen(api_url + "items/all?&" + api_key)
+        marketData = json.load(apiJsonFile)
+        print("Release State = " + str(release_state) + "! API Connected, Market Data Up-To-Date")
 
-    offlineMarketData_Json_File = open("offlineMarketData.json", "w", encoding="utf-8")
-    json.dump(marketData, offlineMarketData_Json_File, ensure_ascii = False)
-    print("Notice: Offline Market Data Updated & Saved\n")
-else:
+        offlineMarketData_Json_File = open("offlineMarketData.json", "w", encoding="utf-8")
+        json.dump(marketData, offlineMarketData_Json_File, ensure_ascii = False)
+        print("Notice: Offline Market Data Updated & Saved\n")
+    except:
+        print("API Connection Error!, Reverting to Offline Data")
+        OfflineMarketData()
+
+def FetchOfflineMarketData():
     offlineMarketData_Json_File = open("offlineMarketData.json", encoding="utf-8")
     marketData = json.load(offlineMarketData_Json_File)
     print("Release State = " + str(release_state) + "!, API Not Connected, Market Data Outdated")
@@ -24,21 +29,31 @@ def RefreshmarketData():
         json_obj = urlopen(api_url + "items/all?&" + api_key)
         marketData = json.load(json_obj)
 
+
+if (release_state):
+    OnlineMarketData()
+else:
+    OfflineMarketData()
+
+
 def itemMarket_In_userMessage(p_marketItemShortName, p_userMessage):
     if (p_marketItemShortName.replace("-", "").replace(" ", "").lower() in p_userMessage.replace("-", "").replace(" ", "").lower()):
         return True
     return False
 
+
 def isItemNamePresent(p_userMessage):
     for marketItem in marketData:
         if (itemMarket_In_userMessage(marketItem["shortName"], p_userMessage)):
             return True
-    return False
+    return False #---Item name not present---
+
 def getItemName(p_userMessage):
     for marketItem in marketData:
         if (itemMarket_In_userMessage(marketItem["shortName"], p_userMessage)):
             return marketItem["shortName"]
-    return "ERROR! ---Item name not present---"
+    return False #---Item name not present---"
+
 def getItemPrice(p_itemName, p_marketType):
     RefreshmarketData()
     for marketItem in marketData:
@@ -49,9 +64,10 @@ def getItemPrice(p_itemName, p_marketType):
                 return marketItem["traderPriceCur"] + str(marketItem["avg24hPrice"])
             else:
                 return "ERROR! ---Item market type not specified---"
-    return "ERROR! ---Item price not found---"
+    return False #---Item price not found---"
+
 def getItemTraderName(p_itemName):
     for marketItem in marketData:
         if (marketItem["shortName"] == p_itemName):
             return marketItem["traderName"]
-    return "ERROR! ---Item not sold by any trader" 
+    return False #---Item not sold by any trader" 
